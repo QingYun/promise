e50ed3557c042f45b280de9cf3b7b7e88bb122f8
QingYun <qingyun95@gmail.com>
2014-04-01 12:44:58 +0800

    添加ExceptionOr

---
+++ promise-test/exception_or_test.cpp
@@ -0,0 +1,103 @@
+#include "exception_or.h"
+#include <gtest/gtest.h>
+
+using namespace promise;
+
+TEST(ExceptionOrTest, Empty) {
+  ExceptionOr<int> eoi{};
+  EXPECT_FALSE(eoi.IsException());
+  EXPECT_FALSE(eoi.IsValue());
+  EXPECT_TRUE(eoi.IsEmpty());
+}
+
+TEST(ExceptionOrTest, SaveException) {
+  {
+    ExceptionOr<int> eoi{
+        std::make_exception_ptr(std::runtime_error("exception"))};
+    EXPECT_TRUE(eoi.IsException());
+    EXPECT_FALSE(eoi.IsValue());
+    EXPECT_FALSE(eoi.IsEmpty());
+    try {
+      std::rethrow_exception(eoi.GetException());
+    }
+    catch (const std::runtime_error& e) {
+      EXPECT_STREQ("exception", e.what());
+      EXPECT_TRUE(eoi.IsEmpty());
+    }
+  }
+
+  {
+    ExceptionOr<int> eoi{};
+    EXPECT_TRUE(eoi.IsEmpty());
+    eoi.SetException(std::make_exception_ptr(std::runtime_error("exception")));
+    EXPECT_TRUE(eoi.IsException());
+    EXPECT_FALSE(eoi.IsValue());
+    EXPECT_FALSE(eoi.IsEmpty());
+    try {
+      std::rethrow_exception(eoi.GetException());
+    }
+    catch (const std::runtime_error& e) {
+      EXPECT_STREQ("exception", e.what());
+      EXPECT_TRUE(eoi.IsEmpty());
+    }
+  }
+}
+
+TEST(ExceptionOrTest, SaveScalarValue) {
+  {
+    ExceptionOr<int> eoi{123};
+    EXPECT_TRUE(eoi.IsValue());
+    EXPECT_FALSE(eoi.IsException());
+    EXPECT_FALSE(eoi.IsEmpty());
+    EXPECT_EQ(123, eoi.GetValue());
+    EXPECT_TRUE(eoi.IsEmpty());
+  }
+
+  {
+    ExceptionOr<int> eoi{};
+    EXPECT_TRUE(eoi.IsEmpty());
+    eoi.SetValue(123);
+    EXPECT_TRUE(eoi.IsValue());
+    EXPECT_FALSE(eoi.IsException());
+    EXPECT_FALSE(eoi.IsEmpty());
+    EXPECT_EQ(123, eoi.GetValue());
+    EXPECT_TRUE(eoi.IsEmpty());
+  }
+}
+
+TEST(ExceptionOrTest, SaveClassValue) {
+  struct C {
+    int value_;
+    explicit C(int value) : value_(value) {}
+
+    C(const C&) = delete;
+    C& operator=(const C&) = delete;
+
+    C(C&& other) : value_(other.value_) {}
+    C& operator=(C&& other) {
+      value_ = other.value_;
+      return *this;
+    }
+  };
+
+  {
+    ExceptionOr<C> eoc{ C{123} };
+    EXPECT_TRUE(eoc.IsValue());
+    EXPECT_FALSE(eoc.IsException());
+    C c = eoc.GetValue();
+    EXPECT_EQ(123, c.value_);
+    EXPECT_TRUE(eoc.IsEmpty());
+  }
+
+  {
+    ExceptionOr<C> eoc{};
+    EXPECT_TRUE(eoc.IsEmpty());
+    eoc.SetValue(C{ 123 });
+    EXPECT_TRUE(eoc.IsValue());
+    EXPECT_FALSE(eoc.IsException());
+    EXPECT_FALSE(eoc.IsEmpty());
+    C c = eoc.GetValue();
+    EXPECT_EQ(123, c.value_);
+    EXPECT_TRUE(eoc.IsEmpty());
+  }
+}
---
+++ promise/exception_or.h
@@ -0,0 +1,90 @@
+#pragma once
+#include <type_traits>
+#include <exception>
+#include <new>
+#include <cassert>
+
+namespace promise {
+
+namespace _ {
+
+template <
+    typename T,
+    bool TIsLarger = (sizeof(typename std::aligned_storage<
+                          sizeof(T), std::alignment_of<T>::value>::type) >
+                      sizeof(typename std::aligned_storage<
+                          sizeof(std::exception_ptr),
+                          std::alignment_of<std::exception_ptr>::value>::type))>
+struct Storage_;
+
+template <typename T>
+struct Storage_<T, true> {
+  using Type = typename std::aligned_storage<sizeof(T),
+                                             std::alignment_of<T>::value>::type;
+};
+
+template <typename T>
+struct Storage_<T, false> {
+  using Type = typename std::aligned_storage<
+      sizeof(std::exception_ptr),
+      std::alignment_of<std::exception_ptr>::value>::type;
+};
+
+template <typename T>
+class Storage {
+  typename Storage_<T>::Type storage_;
+
+ public:
+  template <typename T>
+  void Set(T&& v) {
+    new(&storage_) T{std::move(v)};
+  }
+  template <typename T>
+  T& As() {
+    return *reinterpret_cast<T*>(&storage_);
+  }
+};
+
+}  // namespace _
+
+template <typename T>
+class ExceptionOr {
+  _::Storage<T> storage_;
+
+  enum class State { Empty, Exception, Value } state_;
+
+ public:
+   ExceptionOr() : state_(State::Empty) {}
+   ExceptionOr(T&& value) : state_(State::Value) {
+    storage_.Set(std::move(value));
+  }
+   ExceptionOr(std::exception_ptr exception) : state_(State::Exception) {
+    storage_.Set(std::move(exception));
+  }
+
+   bool IsEmpty() const { return state_ == State::Empty; }
+   bool IsException() const { return state_ == State::Exception; }
+   bool IsValue() const { return state_ == State::Value; }
+
+  void SetException(std::exception_ptr exception) {
+    assert(IsEmpty());
+    storage_.Set(std::move(exception));
+    state_ = State::Exception;
+  }
+  void SetValue(T&& value) {
+    assert(IsEmpty());
+    storage_.Set(std::move(value));
+    state_ = State::Value;
+  }
+  
+  std::exception_ptr GetException() {
+    state_ = State::Empty;
+    return std::move(storage_.As<std::exception_ptr>());
+  }
+  T GetValue() {
+    state_ = State::Empty;
+    return std::move(storage_.As<T>());
+  }
+};
+
+}  // namespace promise
